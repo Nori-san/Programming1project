8928a8d02d9fb7c1b002f60bc5e5f5650f0e41d7
Sang Kyu Kim <sang.kim@johnabbottcollege.net>
2023-06-20 15:42:01 -0400

    Arranged Q2 with a method

--- Q2.java
+++ Q2.java
@@ -6,38 +6,43 @@ public class Q2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-//		You are commissioned to write a Java program that will asking from user integer number and call it joker, Then ask from user how many number he/she wants to put .
-//		After getting all the number output will be as follow, 
-//		If the number is less than joker give the zero otherwise deduct that number until getting number less than joker and keep that number. 
+// 		You are commissioned to write a Java program that will asking from user integer number and call it joker, 
+//		Then ask from user how many number he/she wants to put . After getting all the number output will be as follow, 
+//		If the number is less than joker give the zero otherwise deduct that number until getting number less than joker 
+//		and keep that number.  
 
+		int i = 0, joker, size;
 		Scanner input = new Scanner(System.in);
-		//Jokers input
+		// Jokers input
 		System.out.print("Joker: ");
-		int joker = input.nextInt();
-		//Size of the array
+		joker = input.nextInt();
+		// Size of the array
 		System.out.print("Number: ");
-		int size = input.nextInt();
-		
-		int [] arrayNum = new int[size];
-		
-		//iterating the size of the array then input numbers corresponds to the index
-		for (int i = 0; i < size; i++) {
-            System.out.print("Enter Numbers:  ");
-            int num = input.nextInt();
-            	while ( num >= joker) { //Every number bigger than the joker
-            		num -= joker;	
-            	}
-            	arrayNum[i]=num;
-            }
-		 System.out.println();
-            for (int i = 0; i < size; i++) {
-                System.out.print(arrayNum[i] + " ");
-            }
+		size = input.nextInt();
+
+		int[] arrayNum = new int[size];
+		System.out.print("Enter Numbers:  ");
+
+		do {	
+			arrayNum[i++] = input.nextInt();  // getting input from user N-times according to the "size"
+		} while (i < arrayNum.length);
+
+		jokerCheck(arrayNum, joker);
+
 		input.close();
-		}
 	}
 
+	public static void jokerCheck(int[] arr, int joker) {
+		int i;
 
-	
+		for (i = 0; i < arr.length; i++) {  // compare with joker
+			while (arr[i] >= joker) {		// until joker <= value , repeat the subtraction
+				arr[i] -= joker;
+			}
+		}
+		for (i = 0; i < arr.length; i++) {
+			System.out.print(arr[i] + " ");
+		}
 
+	}
+}
