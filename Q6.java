df841f9c2b2670b606844ff634be10b8d17899ae
Mark Jay <marksumoba@gmail.com>
2023-06-20 09:58:53 -0400

    Question 6 completed

--- Q6.java
+++ Q6.java
@@ -1,10 +1,30 @@
 package Programming1project;
 
+import java.util.Arrays;
+
 public class Q6 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+//		Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is missing from the array.
+		
+		int [] arrayNum = {9,6,4,2,3,5,7,0,1};
+		
+		System.out.println("Original Array " + Arrays.toString(arrayNum));
+		int missingNumber = findMissingNumber(arrayNum);
+		System.out.println("The missing number is: " + missingNumber);
 
 	}
+	
+	public static int findMissingNumber(int[] array) {
+		int n = array.length; 
+		 int sum = (n * (n + 1)) / 2;
+		int actualSum = 0;
+		for (int i = 0; i < n; i++) {
+			actualSum += array[i];
+		}
 
+		return sum - actualSum;
+	}
 }
