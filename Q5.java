10face3d9578853365d874065286285cea95e772
Sang Kyu Kim <sang.kim@johnabbottcollege.net>
2023-06-20 19:06:33 -0400

    Revise Q5 to get user input

--- Q5.java
+++ Q5.java
@@ -1,26 +1,29 @@
 package Programming1project;
 
-import java.util.Random;
 import java.util.Scanner;
 
 public class Q5 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int i, sz, majorNum;
+		// Given an array nums of size n, return the majority element. The majority
+		// element is the element that appears more than [n / 2] times. You may assume
+		// that the majority element always exists in the array.
+
+		int i = 0, sz, majorNum;
 
 		Scanner kb = new Scanner(System.in);
-		Random rand = new Random();
-		System.out.println("Please Input the size of array to creat  : ");
+		System.out.println("How many numbers do you want to enter? ");
 		sz = kb.nextInt();
-		kb.close();
 
 		int[] myarr1 = new int[sz];
+		System.out.println("Please enter numbers :");
+		do {
+			myarr1[i++] = kb.nextInt();
+		} while (i < myarr1.length);
 
-		for (i = 0; i < myarr1.length; i++) {
-			myarr1[i] = rand.nextInt(1,4);
-		}
-
+		kb.close();
+//		int[] myarr2 = { 2, 2, 1, 1, 1, 2, 2 };
 		System.out.println("============================ ");
 		System.out.println("Printing the array ");
 		System.out.println("============================ ");
@@ -29,7 +32,7 @@ public class Q5 {
 		}
 		System.out.println();
 
-		majorNum = majorElement(myarr1);  // Call the method to check the majority element
+		majorNum = majorElement(myarr1); // Call the method to check the majority element
 		System.out.println("==========================");
 		System.out.println("Major num : " + majorNum);
 
@@ -38,43 +41,36 @@ public class Q5 {
 	public static int majorElement(int[] arr) {
 		int i, j, majorNum = 0, numCnt = 1, temp = 0, dupCnt = 0;
 
-		for (i = 0; i < arr.length; i++) {   // Element Count in array 
+		for (i = 0; i < arr.length; i++) { // count the number of element
 			temp = arr[i];
 			for (j = i + 1; j < arr.length; j++) {
-				if (i > 0 && arr[i] == arr[j]) {
-					break;
+				if (arr[j] == arr[i]) {
+					numCnt++;
 				} else {
-					if (arr[j] == temp) {
-						numCnt++;
-					} else {
-						continue;
-					}
-				}
-			}
-			if (i > 0 && arr[i] == arr[i - 1]) { // Filtering duplicates to print out
-				continue;
-			} else {
-				for (j = i; j >= 0; j--) {
-					if (arr[j] == temp) {
-						dupCnt++;
-					} else {
-						continue;
-					}
-				}
-				if (dupCnt < 2) {
-					System.out.println("number : " + temp);
-					System.out.println("number count : " + numCnt);
-					if (numCnt > arr.length / 2) {
-						majorNum = temp;
-					}
-					numCnt = 1;
-					dupCnt = 0;
-				} else {
-					dupCnt = 0;
 					continue;
 				}
 			}
+			
+			for (j = i; j >= 0; j--) { // filtering duplicate of element
+				if (arr[j] == temp) {
+					dupCnt++;
+				} else {
+					continue;
+				}
+			}
+
+			if (dupCnt <= 1) {
+				System.out.println(arr[i] + " is counted " + numCnt + " time(s).");
+			}
+
+			if (dupCnt > arr.length / 2) {
+				majorNum = temp;
+			}
+			
+			numCnt = 1;
+			dupCnt = 0;
 		}
+
 		return majorNum;
 	}
 
